42c5a031cbf1efacfabb3f1c47e599693c0b75ff
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-06-07 18:55:50 +0200

    fix region names typo

--- src/main/scala/ohnosequences/saws/regions.scala
+++ src/main/scala/ohnosequences/saws/regions.scala
@@ -5,7 +5,7 @@ sealed trait RegionAux {
   val name: String
 }
 case object EU extends RegionAux { val name = "eu-west-1" }
-case object US extends RegionAux { val name = "us_east-1" }
-case object US_WEST_1 extends RegionAux { val name = "us_west-1" }
-case object US_WEST_2 extends RegionAux { val name = "us_west-2" }
-case object US_WEST_3 extends RegionAux { val name = "us_west-3" }
+case object US extends RegionAux { val name = "us-east-1" }
+case object US_WEST_1 extends RegionAux { val name = "us-west-1" }
+case object US_WEST_2 extends RegionAux { val name = "us-west-2" }
+case object US_WEST_3 extends RegionAux { val name = "us-west-3" }
